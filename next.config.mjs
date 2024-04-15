097055cc300d6e410a560050876b57e0c005fa6a
vagnermengali <vagmengali2015@gmail.com>
2024-04-15 02:09:26 -0300

    feat: adding full and responsive pop up

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compress: true,
+  swcMinify: true,
+  optimizeFonts: true,
+  compiler: {
+    removeConsole: true,
+  },
 };
 
 export default nextConfig;
