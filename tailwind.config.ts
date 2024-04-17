4d6f474806cfcf97b4c2c255238f9db6bea077dc
vagnermengali <vagmengali2015@gmail.com>
2024-04-17 15:37:09 -0300

    feat: adding new changes for SEO improvement

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,7 +87,7 @@ const config: Config = {
         "minicart-out": "minicart-out 0.5s forwards",
       },
       backgroundImage: {
-        "arrow-x": "url('https://github.com/vagnermengali/maeztra-challenge/blob/main/public/assets/icons/arrowX.svg')",
+        "arrow-x": "url('https://raw.githubusercontent.com/vagnermengali/maeztra-challenge/e22d6ba91a3e4c6f5693b2f85c85f064d672c582/public/assets/icons/arrowX.svg')",
       },
     },
   },
